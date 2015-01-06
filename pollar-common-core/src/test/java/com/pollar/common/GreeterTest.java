36ea1b45da85990f7cd88e24277a5e76c27614ea
Eric Yang <eric@exprate.com>
2015-01-06 00:42:58 -0800

    Add inter-module maven dependency and import logging libaries, and fix JUnit version

---
+++ pollar-common-core/src/test/java/com/pollar/common/GreeterTest.java
@@ -0,0 +1,14 @@
+package com.pollar.common;
+
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+public class GreeterTest {
+
+    public void testSayHello() {
+        assertEquals("hello", new Greeter().sayHello());
+    }
+}
---
+++ pollar-domain-model/src/test/java/com/pollar/domain/AppTest.java
@@ -0,0 +1,16 @@
+package com.pollar.domain;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    public void testApp() {
+        assertTrue(true);
+    }
+}
